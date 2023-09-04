a22f46fbb46780983cd495b138c2a6c333c1c1a1
Mattch06 <matthewchristiansen06@gmail.com>
2023-09-04 13:50:03 -0500

    w/ aws integration

--- checkSubs.py
+++ checkSubs.py
@@ -6,10 +6,23 @@ from dotenv import load_dotenv
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.header import Header
+import boto3
+from botocore.exceptions import ClientError
 import psycopg2
 import os
 import json
 import time
+
+def get_aws_secret(secret_name):
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name='us-east-1')
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        raise e
+
+    return get_secret_value_response['SecretString']
 
 def main():
 
@@ -35,8 +48,18 @@ def main():
         smt = smtplib.SMTP('smtp.gmail.com', 587)
         smt.ehlo()
         smt.starttls()
-        email = os.environ.get('SENDER_EMAIL')
-        password = os.environ.get('SENDER_PASSWORD')
+        
+        # Retrieve SENDER_EMAIL and SENDER_PASSWORD secrets
+        try:
+            secret_data = get_aws_secret("gpuscrapesite/sender_email")
+            email = secret_data.get("SENDER_EMAIL")
+            password = secret_data.get("SENDER_PASSWORD")
+        except KeyError as e:
+            print(f"KeyError: {e}. Please ensure the secrets in AWS Secrets Manager have the correct keys.")
+            return
+
+        smt.login(email, password)
+
         smt.login(email, password)
 
         # Get subscription data using SQLAlchemy query
--- run_webscrape.py
+++ run_webscrape.py
@@ -2,11 +2,17 @@ import os
 import scrape_tools.webscrape as webscrape
 import json
 import time
+import boto3
 
-def read_urls_from_file(file_path):
-    with open(file_path, 'r') as file:
-        urls = [line.strip() for line in file]
-    return urls
+def read_urls_from_s3(bucket_name, file_name):
+    s3 = boto3.client('s3')
+    try:
+        response = s3.get_object(Bucket=bucket_name, Key=file_name)
+        urls = [line.decode('utf-8').strip() for line in response['Body'].iter_lines()]
+        return urls
+    except Exception as e:
+        print(f"Error reading URLs from S3: {e}")
+        return []
 
 def run_webscrape():
     config_path = 'config.json'  # Adjust the path to reach the config.json file
@@ -16,9 +22,17 @@ def run_webscrape():
         config = json.load(config_file)
 
     db_uri = config[env]['DATABASE_URI']
-    urls = read_urls_from_file('urls.txt')
+    bucket_name = 'mattch06-webscraping-data'
+    file_name = 'urls.txt'
+    
+    urls = read_urls_from_s3(bucket_name, file_name)
+    
+    if not urls:
+        print(f"No URLs found in S3 bucket: {bucket_name}/{file_name}")
+        return
+    
     webscrape.scrape_and_update(urls, db_uri)
-        
+    
     time.sleep(86400)  # 1 day
 
 if __name__ == "__main__":
